b1d4100c4edfc86eea78578a46d4cfb855554cff
Yann SEGET <dev@leafgard.fr>
2022-03-01 13:48:52 +0100

    feat: made api return container objects

--- src/api/Containers.ts
+++ src/api/Containers.ts
@@ -1,13 +1,16 @@
+import { Container, ContainerSummary } from '../classes/Container.js'
 import { jsonEndpoint } from '../DockerAPI.js'
 import { GetParamType } from '../utils/GetParamType.js'
 import { GetResponseType } from '../utils/GetResponseType.js'
 
 export class Containers {
-  listContainers(query?: GetParamType<'ContainerList'>['query']) {
-    return jsonEndpoint<GetResponseType<'ContainerList', 200>>(
-      'get',
-      'containers/json',
-      { searchParams: query }
+  async listContainers(query?: GetParamType<'ContainerList'>['query']) {
+    const containers = await jsonEndpoint<
+      GetResponseType<'ContainerList', 200>
+    >('get', 'containers/json', { searchParams: query })
+    return containers.map(
+      (containerConfig) =>
+        new Container(containerConfig as Required<ContainerSummary>)
     )
   }
 }
--- src/classes/Container.ts
+++ src/classes/Container.ts
@@ -1,1 +1,31 @@
-export class Container {}
+import { jsonEndpoint } from '../DockerAPI.js'
+import { definitions } from '../specs/v1.41.js'
+
+export type ContainerSummary = definitions['ContainerSummary']
+type Mount = definitions['Mount']
+type EndpointSettings = definitions['EndpointSettings']
+type Port = definitions['Port']
+
+export class Container implements Required<ContainerSummary> {
+  public Command!: string
+  public Created!: number
+  public HostConfig!: { NetworkMode?: string }
+  public Id!: string
+  public Image!: string
+  public ImageID!: string
+  public Labels!: { [p: string]: string }
+  public Mounts!: Mount[]
+  public Names!: string[]
+  public NetworkSettings!: {
+    Networks: { [p: string]: EndpointSettings }
+  }
+  public Ports!: Port[]
+  public SizeRootFs!: number
+  public SizeRw!: number
+  public State!: string
+  public Status!: string
+
+  constructor(props: Required<ContainerSummary>) {
+    Object.assign(this, props)
+  }
+}
