d8a909adb3645573c1e067c3af08ffc036370b40
Yann SEGET <dev@leafgard.fr>
2022-03-05 17:28:24 +0100

    feat: add container inspect and top self methods

--- src/classes/Container.ts
+++ src/classes/Container.ts
@@ -1,5 +1,7 @@
 import { jsonEndpoint } from '../DockerAPI.js'
 import { definitions } from '../specs/v1.41.js'
+import { GetParamType } from '../utils/GetParamType.js'
+import { GetResponseType } from '../utils/GetResponseType.js'
 
 export type ContainerSummary = definitions['ContainerSummary']
 type Mount = definitions['Mount']
@@ -27,5 +29,33 @@ export class Container implements Required<ContainerSummary> {
 
   constructor(props: Required<ContainerSummary>) {
     Object.assign(this, props)
+  }
+
+  /**
+   * Inspect a container
+   * @description Return low-level information about a container.
+   */
+  inspect(query?: GetParamType<'ContainerInspect'>['query']) {
+    return jsonEndpoint<GetResponseType<'ContainerInspect', 200>>(
+      'get',
+      `containers/${this.Id}/json`,
+      {
+        searchParams: query
+      }
+    )
+  }
+
+  /**
+   * List processes running inside a container
+   * @description On Unix systems, this is done by running the ps command. This endpoint is not supported on Windows.
+   */
+  top(query?: GetParamType<'ContainerTop'>['query']) {
+    return jsonEndpoint<GetResponseType<'ContainerTop', 200>>(
+      'get',
+      `containers/${this.Id}/top`,
+      {
+        searchParams: query
+      }
+    )
   }
 }
